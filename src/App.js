c2586c2afa91fa7030b629fb2aa82ba8e4ffa861
Utkarsh <utkarshsingh341@gmail.com>
2022-04-12 19:41:54 +0530

    All components added

--- src/App.js
+++ src/App.js
@@ -5,57 +5,19 @@ import './App.css';
 import './brands.css';
 import Navbar from './Components/navbar';
 import Brands from './Components/brands';
+import Slider from './Components/slider';
 import Cards from './Components/cards';
+import Footer from './Components/footer';
 
 function App() {
   return (
     <div className="App">
-      <Navbar/>
-{/* 
-      <div id="carouselExampleDark" class="carousel ccd carousel-dark slide" data-bs-ride="carousel">
-        <div class="carousel-indicators">
-          <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-          <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="1" aria-label="Slide 2"></button>
-          <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="2" aria-label="Slide 3"></button>
-        </div>
-        <div class="carousel-inner">
-          <div class="carousel-item active" data-bs-interval="10000">
-            <img src={require('./img/img1.png')} class="d-block w-100" alt="..." className="images-css"/>
-            <div class="carousel-caption d-none d-md-block">
-              <h5>First slide label</h5>
-              <p>Some representative placeholder content for the first slide.</p>
-            </div>
-          </div>
-          <div class="carousel-item" data-bs-interval="2000">
-            <img src={require('./img/img2.png')} class="d-block w-100" alt="..." className="images-css" />
-            <div class="carousel-caption d-none d-md-block">
-              <h5>Second slide label</h5>
-              <p>Some representative placeholder content for the second slide.</p>
-            </div>
-          </div>
-          <div class="carousel-item">
-            <img src={require('./img/img3.png')} class="d-block w-100" alt="..." className="images-css" />
-            <div class="carousel-caption d-none d-md-block">
-              <h5>Third slide label</h5>
-              <p>Some representative placeholder conten</p>
-            </div>
-          </div>
-        </div>
-        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="prev">
-          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-          <span class="visually-hidden">Previous</span>
-        </button>
-        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="next">
-          <span class="carousel-control-next-icon" aria-hidden="true"></span>
-          <span class="visually-hidden">Next</span>
-        </button>
-      </div> */}
-
+      
+<Navbar/>
+<Slider/>
 <Brands/>
 <Cards/>
-
-
-
+<Footer/>
 
     </div>
   );
